4e8519bc1fc70e0a84f2973ae140d1023db36409
Krishna Kumar <149694348+KRISHNAKUMARPS2002@users.noreply.github.com>
2025-06-06 17:30:46 +0530

    HOSTS added

--- rrc/settings.py
+++ rrc/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'rrcpython.imcbs.com']
 
 # Application definition
 INSTALLED_APPS = [
